54f3c8b01bf367008f607cccb70871d0c0f7b0c0
hitesh-manwani <109348270+hitesh-manwani@users.noreply.github.com>
2022-08-15 16:06:26 +0530

    Update stop.sh

--- script/stop.sh
+++ script/stop.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-apt update -y
+echo "Stoping Server!!! first check if pm2 is sruuning or not"
+status=$(sudo pm2 PID app)
+if [ $status == 0 ]; then
+ echo "pm2 is not running go to install "
+else
+  cd /home/ubuntu/node-express-realworld-example-app
+  pm2 stop app.js
+  echo "Server Stopped!!"
+fi
